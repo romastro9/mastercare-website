72609c8c14570111bef0beb96bf707202b39a59f
romastro9 <athikacg12@gmail.com>
2026-07-06 00:22:39 +0700

    Add version check endpoint

---
+++ version.php
@@ -0,0 +1,5 @@
+<?php
+header('Content-Type: text/plain; charset=utf-8');
+echo "MasterCare Website v2.0\n";
+echo "Updated: 2026-07-06\n";
+echo "If you see this file on Anajak, the new GitHub files are uploaded.\n";
