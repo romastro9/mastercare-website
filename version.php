572633eb241b1ec44e9dd5d855bcca98c10da0e3
romastro9 <athikacg12@gmail.com>
2026-07-06 00:35:45 +0700

    version

--- version.php
+++ version.php
@@ -1,5 +1,3 @@
 <?php
 header('Content-Type: text/plain; charset=utf-8');
-echo "MasterCare Website v2.0\n";
-echo "Updated: 2026-07-06\n";
-echo "If you see this file on Anajak, the new GitHub files are uploaded.\n";
+echo "v3\n";
